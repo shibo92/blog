84d4195e56ba834270646789fdbecdf3c33eade5
shibo <shibo92@qq.com>
2018-11-01 22:37:26 +0800

    add config repository

--- comment/src/main/java/com/shibo/controller/CommentsController.java
+++ comment/src/main/java/com/shibo/controller/CommentsController.java
@@ -1,6 +1,7 @@
 package com.shibo.controller;
 
 import com.shibo.common.JsonResult;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("comments")
 public class CommentsController {
+
+    @Value("${hehe.haha}")
+    private String configContent;
+
     @GetMapping(value = "/{commentId}")
-    public JsonResult get(@PathVariable(value = "commentId") Integer commentId){
-        return JsonResult.getSuccessfulResult("get a comments:" + commentId);
+    public JsonResult get(@PathVariable(value = "commentId") Integer commentId) {
+        return JsonResult.getSuccessfulResult("get a comments:" + commentId + " and config content = " + configContent);
     }
 }

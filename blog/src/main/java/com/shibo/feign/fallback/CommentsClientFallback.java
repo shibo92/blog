8ff3159ae687ea74611f3e29e2873dd407cc1547
shibo <shibo92@qq.com>
2018-11-04 16:24:40 +0800

    add fallback for feign

--- blog/src/main/java/com/shibo/feign/fallback/CommentsClientFallback.java
+++ blog/src/main/java/com/shibo/feign/fallback/CommentsClientFallback.java
@@ -2,21 +2,20 @@ package com.shibo.feign.fallback;
 
 import com.shibo.common.JsonResult;
 import com.shibo.feign.client.CommentsClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
+/**
+ * @author shibo
+ */
+@Slf4j
 @Component
-public class CommentsClientFallback extends BaseFallBack<CommentsClient> {
-
+public class CommentsClientFallback implements CommentsClient {
     @Override
-    public CommentsClient create(Throwable cause){
-        return new CommentsClient() {
-            @Override
-            public JsonResult get(Integer commentId) {
-                fallbackTriggered(cause);
-                return JsonResult.getSuccessfulResult();
-            }
-        };
+    public JsonResult get(Integer commentId) {
+        log.error("CommentsClient获取数据异常");
+        return JsonResult.getSuccessfulResult();
     }
 }

da66322425064ae5410e8ca6e50653183ed743d6
shibo <shibo92@qq.com>
2018-11-06 14:31:26 +0800

    remove spring security

--- blog/src/main/java/com/shibo/controller/UserController.java
+++ blog/src/main/java/com/shibo/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.shibo.controller;
 
 import com.shibo.common.JsonResult;
-import com.shibo.entity.User;
 import com.shibo.feign.client.CommentsClient;
-import com.shibo.security.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +21,5 @@ public class UserController {
     @ResponseBody
     public JsonResult get() {
         return commentsClient.get(100);
-    }
-
-    @GetMapping("/current")
-    @ResponseBody
-    public JsonResult testCurrentUser(@CurrentUser User user) {
-        System.out.println(user);
-        return JsonResult.getSuccessfulResult(user);
     }
 }

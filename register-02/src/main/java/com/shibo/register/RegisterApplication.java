20fab0c7f93e4d22197f600e6b399ac0fd8dfbaf
shibo <shibo92@qq.com>
2019-04-28 17:16:56 +0800

    eureka集群

---
+++ register-02/src/main/java/com/shibo/register/RegisterApplication.java
@@ -0,0 +1,16 @@
+package com.shibo.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shibo
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+}
